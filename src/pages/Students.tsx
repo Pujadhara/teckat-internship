9cdeedfbe56c3c99fc6f9421b08a200e49e03c4e
Pujadhara <dharapuja54@gmail.com>
2024-06-15 18:29:29 +0530

    homepage

---
+++ src/pages/Students.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Students:React.FC = () => {
+  return <>update</>
+};
+
+export default Students;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import CreateStudent from "./CreateStudent";
+import StudentOutlet from "./StudentOutlet";
+import Students from "./Students";
+import UpdateStudent from "./UpdateStudent";
+
+export {HomePage, StudentOutlet, Students, CreateStudent, UpdateStudent};
